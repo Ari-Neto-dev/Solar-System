6e1c2d5d3ce3807d214ef01518ef4b584c65003a
Ari Azevedo <azevedo.consultor@hotmail.com>
2025-01-05 21:34:54 -0300

    integração total com API

--- src/pages/planet.js
+++ src/pages/planet.js
@@ -1,20 +1,68 @@
 import { Button, Container, Stack, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ShareIcon from '@mui/icons-material/Share';
-import { ArrowForward } from "@mui/icons-material";
+import { Component, useEffect, useState } from "react";
+import ComponentsLayoutStandard from "../components/layout/standard";
+import ApiPlanetas from "../api/planetas";
+// import { ArrowForward } from "@mui/icons-material";
 
 export default function PagePlanet(){
-  function ClickCompartilhar(){
+  const parametros=useParams();
+  const [carregando, setCarregando] = useState(true);
+
+  const [erro, setErro] = useState(null);
+
+  const [message, setMessage] = useState(null);
+
+  const [planeta, setPlaneta] = useState([]);
+
+    //função que executa quando é aberto a página//
+    useEffect(() => {
+      (async () => {
+
+          try {
+                  let respostaApi = await ApiPlanetas.obterUnicoPorSlug(parametros.slug);
+                  if(!respostaApi[0]){throw new Error("Não encontramos o planeta")}
+                  setPlaneta(respostaApi?.[0]);
+                  setCarregando(false);
+                } catch (error) {
+                  setErro({
+                    subtitulo: `Ocorreu um erro em nossa API paa obter os planetas, tente novamente mais tarde.`,
+                    descricao: String(error)
+                  });
+                  setCarregando(false);
+                }
+
+
+
+
+      })()
+    }, []);
+
+
+
+
+  async function ClickCompartilhar(){
+    let resImg=await fetch(planeta?.imgUrl);
+    let contentType= resImg.headers.get("content-type");
+    let blob = await resImg.blob();
+    let binaryFile = new File([blob], `${planeta?.slug}-${planeta?.img}`, { type: contentType });
+
+
+
     window.navigator.share({
       url:`${window.location.href}`,
-      text:`Explore o planeta Nome e descubra suas peculiaridades`,
-      title: `Conheça NOME`
+      text:`Explore o planeta ${planeta?.nome} e descubra suas peculiaridades`,
+      title: `Conheça ${planeta?.nome} `,
+      files:Array(binaryFile),
     })
 
   }
   return(
-    <>
+    <ComponentsLayoutStandard  carregando={carregando}erro={erro} message={message}
+        >
+
      <Container maxWidth="xl" className="containerMargin">
      <Link to="/planets">
           <Button variant="outlined" size="large" ><ArrowBackIosIcon/>
@@ -30,18 +78,19 @@ export default function PagePlanet(){
           alignItems: "center",
         }}
       >
-      <img className="animacaoEfeitoGirar" src="" alt=""/>
-        <Typography variant="h5" component="h1">Terra</Typography>
-        <Typography variant="body1" component="p">Descrição do Planeta Terra.</Typography>
+      <img className="animacaoEfeitoGirar" src={planeta?.imgUrl} alt={planeta?.descrição}/>
+        <Typography variant="h5" component="h1">{planeta?.nome}</Typography>
+        <Typography variant="body1" component="p">{planeta?.descrição}</Typography>
         <Button variant="contained" size="large" endIcon={<ShareIcon/>} onClick={()=>{ClickCompartilhar(); }}>
             Compartilhar
           </Button>
         </Stack>
 
       </Container>
+      </ComponentsLayoutStandard>
 
 
-    </>
+
   )
 
 }
--- src/pages/planets.js
+++ src/pages/planets.js
@@ -1,4 +1,10 @@
-import { Card,CardActionArea, CardContent, Container,Grid2,Typography,
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Container,
+  Grid2,
+  Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -6,74 +12,62 @@ import ComponentsLayoutStandard from "../components/layout/standard";
 import ApiPlanetas from "../api/planetas";
 
 export default function PagesPlanets() {
+  const [carregando, setCarregando] = useState(true);
 
-  const[carregando, setCarregando]= useState(true);
+  const [erro, setErro] = useState(null);
 
-  const[erro, setErro]= useState(null);
+  const [message, setMessage] = useState(null);
 
-  const[message, setMessage]= useState(null);
+  const [planetas, setPlanetas] = useState([]);
 
-  const[planetas, setPlanetas]= useState([]);
-
-//função que executa quando é aberto a página//
-  useEffect(()=>{
-    (async()=>{
+  //função que executa quando é aberto a página//
+  useEffect(() => {
+    (async () => {
       try {
         let respostaApi = await ApiPlanetas.obterTodos();
-        setPlanetas(respostaApi)
+        setPlanetas(respostaApi);
         setCarregando(false);
-
-
       } catch (error) {
         setErro({
-          subtitulo:`Ovcorreu um erro em nossa API paa obter os planetas, tente novamente mais tarde.`
-
-
-        })
+          subtitulo: `Ovcorreu um erro em nossa API paa obter os planetas, tente novamente mais tarde.`,
+        });
         setCarregando(false);
-
-
       }
-
-    })()
-
-  },[]);
-
-
-
+    })();
+  }, []);
 
   return (
-    <ComponentsLayoutStandard carregando={carregando} erro={erro} message={message}>
+    <ComponentsLayoutStandard
+      carregando={carregando}
+      erro={erro}
+      message={message}
+    >
       <Container maxWidth="xl" className="containerMargin">
         <Typography variant="h2" component="h1" textAlign="center">
           Planetas do Sistema Solar!
         </Typography>
 
-
-
         <Grid2 container spacing={2} paddingTop={2}>
-
-        {
-        planetas.map((planeta)=>(
-          <Grid2 item xs={6} sm={4} md={3} lg={2} xl={2}>
-          <Card className="cardVidro">
-            <Link to={(`/planet/${planeta?.slug}`)}>
-              <CardActionArea>
-                <CardContent>
-                  <img src={planeta?.imgUrl} alt={planeta?.descrição} width="100%" />
-                  <Typography variant="h5" component="h2">
-                   {planeta?.nome}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Link>
-          </Card>
-        </Grid2>
-
-        ))
-      }
-
-
+          {planetas.map((planeta) => (
+            <Grid2 item xs={6} sm={4} md={3} lg={2} xl={2}>
+              <Card className="cardVidro">
+                <Link to={`/planet/${planeta?.slug}`}>
+                  <CardActionArea>
+                    <CardContent>
+                      <img
+                        src={planeta?.imgUrl}
+                        alt={planeta?.descrição}
+                        width="100%"
+                      />
+                      <Typography variant="h5" component="h2">
+                        {planeta?.nome}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Link>
+              </Card>
+            </Grid2>
+          ))}
         </Grid2>
       </Container>
     </ComponentsLayoutStandard>
